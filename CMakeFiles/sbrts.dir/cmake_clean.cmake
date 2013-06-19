e9d99b6f79785e509e93f81e4961c69563a76310
kompote <tomtomfatcat@gmail.com>
2013-06-19 23:17:06 +0200

    CMake add - part 2

---
+++ CMakeFiles/sbrts.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sbrts.dir/src/main.cpp.o"
+  "sbrts.pdb"
+  "sbrts"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sbrts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ bin/config.h
@@ -0,0 +1,2 @@
+#define sbrts_VERSION_MAJOR 0
+#define sbrts_VERSION_MINOR 0
