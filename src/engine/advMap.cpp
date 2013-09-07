ce666327f6b17170dc978ef231fab8e5ceec0ae7
kompote <tomtomfatcat@gmail.com>
2013-09-07 04:03:32 +0200

    [NEW] new map, introduce 2.5d, tileset method, 100x much faster than old map ^^

---
+++ src/engine/advMap.cpp
@@ -0,0 +1,92 @@
+#include "advMap.h"
+#include<iostream> 
+#include<fstream> 
+#include<cctype>
+#include<string>
+#include<vector>
+
+
+AdvMap::AdvMap()
+{}
+
+AdvMap::~AdvMap()
+{}
+
+// ------------------------------------------------------------------- LoadMap
+// To load a map
+// map name = map install folder
+// return an error if load failed
+unsigned char AdvMap::loadMap(string mapName)
+{
+    // construct path
+    // TODO : global path define
+    string path = "maps/"+ mapName + "/map";
+
+    cout<<"Loading "<<path<<"... ";
+    std::ifstream openfile(path);
+
+    // map representation
+    std::vector<sf::Vector2i> tempMap;
+
+    if(openfile.is_open())
+    {
+        // load tileset
+        std::string tileLocation; 
+        openfile >> tileLocation; 
+        tileTexture.loadFromFile(path+tileLocation); 
+        m_tiles.setTexture(tileTexture);
+
+        // read map file
+        while(!openfile.eof())
+        {
+            // load tiles coordinates
+            std::string str;
+            openfile >> str; 
+            char x = str[0], y = str[2];
+            if(!isdigit(x) || !isdigit(y))
+                tempMap.push_back(sf::Vector2i(-1, -1));
+            else
+                // hack to translate char to int
+                tempMap.push_back(sf::Vector2i(x - '0', y - '0')); 
+
+            // next line
+            if(openfile.peek() == '\n')
+            {
+                m_map.push_back(tempMap); 
+                tempMap.clear();
+            }
+        }
+        m_map.push_back(tempMap);
+
+        cout<<"Done."<<endl;
+        return (unsigned char)0;
+    }
+    cout<<"Falied."<<endl;
+    return (unsigned char)1;
+}
+
+// ----------------------------------------------------------------- redraw
+void AdvMap::redraw(RenderTarget& window)
+{
+
+    for(int i = 0; i < m_map.size(); i++)
+    {
+        for(int j = 0; j < m_map[i].size(); j++)
+        {
+            if(m_map[i][j].x != -1 && m_map[i][j].y != -1)
+            {
+                // offset on unpair
+                // position on window
+                if(i%2==0)
+                    m_tiles.setPosition(j * 30, i * 8);
+                else
+                    m_tiles.setPosition((j * 30)+15, (i * 8));
+
+                // select tile in tileset
+                m_tiles.setTextureRect(sf::IntRect(m_map[i][j].x * 30, m_map[i][j].y * (20+4) , 30, 20)); 
+//              cout<<" coord map : " << j*16 << ":" << i*16 << "   tile coords : " << m_map[i][j].x*32 << ":" <<  m_map[i][j].y*32 << endl;
+                window.draw(m_tiles);
+            }
+        }
+    }
+}
---
+++ src/engine/advMap.h
@@ -0,0 +1,23 @@
+#ifndef ADVMAP_H
+#define ADVMAP_H
+
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+
+class AdvMap
+{
+    public:
+        AdvMap();
+        ~AdvMap();
+        unsigned char loadMap(string filename);
+        void redraw(RenderTarget&);
+    private:
+
+    std::vector<std::vector<sf::Vector2i>> m_map;
+    sf::Sprite m_tiles;
+    sf::Texture tileTexture;
+
+};
+
+#endif
