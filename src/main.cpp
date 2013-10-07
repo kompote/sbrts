52076539b2607d6df1ea3b8d390cb520bbfa91be
kompote <tomtomfatcat@gmail.com>
2013-10-08 01:16:27 +0200

    [NEW] independent game speed / fps

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 #include <list>
-#include "sys/sysinfo.h"
 
 #include "engine/advMap.h"
 #include "engine/unit.h"
@@ -9,11 +8,15 @@
 
 #include <SFML/Graphics.hpp>
 
+const int TICKS_PER_SECOND = 25;  // game speed
+const int SKIP_TICKS = 1000 / TICKS_PER_SECOND; // second ratio
+const int MAX_FRAMESKIP = 5;  // max game update without draw
 
 // global vars
 sf::RenderWindow mainWindow(sf::VideoMode(800, 600), "SBRTS");    // global main window
 AdvMap theMap;
 
+// ----------------------------------------------------------------- toggleFullscreen
 bool bfullscreen = false;
 void toggleFullscreen() {
     if (!bfullscreen)
@@ -23,20 +26,25 @@ void toggleFullscreen() {
     bfullscreen = !bfullscreen;
 
 }
-float getFPS(const sf::Time& time) {
+// ----------------------------------------------------------------- getFPS
+inline float getFPS(const sf::Time& time) {
          return (1000000.0f / time.asMicroseconds());
 }
 //float targetFPS = 60.0;
 //float targetFrameTime = targetFPS*1000000.0f;
+// ----------------------------------------------------------------- main
 int main()
 {
-  DBG_INFO("Starting main");
-   DBG_WARN("Starting main");
+
+    DBG_INFO("Starting main");
+    DBG_WARN("Starting main");
     DBG_ERROR("Starting main");
 
-  // fps control
+    // Gamespeed control
+    sf::Clock GameClock;
+
+    //fps control
     sf::Clock FPSClock;
-//    float fps;
     std::list<float> frameTime;
     unsigned int framecount = 0;
     // Debug stuff
@@ -46,20 +54,25 @@ int main()
     sf::Text _DebugFPS("fps", _DebugFont,12);
     _DebugFPS.setPosition(750.0,5.0);
 
+    // load the map
     theMap.loadMap("map01");
     sf::Sprite mapSprite;
 
+    // first unit test
+    Unit unit;
 
-  Unit unit;
-
-   // map texture offscreen
+    // map texture offscreen
     sf::RenderTexture rt;
     rt.create(1800,1600);
+    // map display position
     int mapPosX = 0, mapPosY = 0;
+
+    // calculate game next update
+    long next_game_tick = GameClock.getElapsedTime().asMilliseconds();
+    int loops;
 
     while (mainWindow.isOpen())
     {
-        // keep real frame time
         // Event management
         sf::Event event;
         while (mainWindow.pollEvent(event))
@@ -100,26 +113,37 @@ int main()
                     break;
             }
         }
+        //update game state when needed
+        loops = 0;
+        while( GameClock.getElapsedTime().asMilliseconds() > next_game_tick && loops < MAX_FRAMESKIP)
+        {
+            // render map
+            rt.clear(sf::Color(20,20,20));
+            theMap.redraw(rt);
+            // render unit
+            unit.Redraw(rt);
+            // bug001
+            rt.draw(sf::Text("toto",_DebugFont,12)); // if not, bug ??
+            // Clear display
+            mainWindow.clear(sf::Color(0, 140, 155));
 
-        // render map
-        rt.clear(sf::Color(20,20,20));
-        theMap.redraw(rt);
-        unit.Redraw(rt);
-        rt.draw(sf::Text("toto",_DebugFont,12)); // if not, bug ??
-        rt.display();
-        // Clear display
-        mainWindow.clear(sf::Color(0, 140, 155));
-        // dram map, just a part
-        // TODO : fulscreen
-        mapSprite.setTexture(rt.getTexture());
-        mapSprite.setTextureRect(sf::IntRect(mapPosX,mapPosY,800,500));
-        mainWindow.draw(mapSprite);
+            // dram map, only displayable part
+            // TODO : fulscreen
+            mapSprite.setTexture(rt.getTexture());
+            mapSprite.setTextureRect(sf::IntRect(mapPosX,mapPosY,800,500));
+            mainWindow.draw(mapSprite);
+            // draw fps text
+            mainWindow.draw(_DebugFPS);
 
-        // draw fps text
-        mainWindow.draw(_DebugFPS);
+            // set next update tick
+            next_game_tick += SKIP_TICKS;
+            loops++;
+
+        }
 
         ++framecount;
 
+        rt.display();
         mainWindow.display();
 
         frameTime.push_back(getFPS(FPSClock.restart()));
